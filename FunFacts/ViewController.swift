25e5e5cfd20d6091aab052c129f048375503b84e
Francisco Ozuna <ozuna252@hotmail.com>
2019-12-28 23:09:36 -0500

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Francisco Ozuna on 12/28/19.
+//  Copyright © 2019 Francisco Ozuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
