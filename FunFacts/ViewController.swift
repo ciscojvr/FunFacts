19a0b7804d747e044a4d7ec5387d5283ac981028
Francisco Ozuna <ozuna252@hotmail.com>
2019-12-30 15:35:37 -0500

    Created IBOutlets in ViewController to link view elements.

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
